8a42bec77fa87d8a9a87c114eea1b07e4a9e2ce7
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-10-10 12:54:17 +0100

    Authenticate with AzAPI

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,3 +6,10 @@ provider "azurerm" {
   tenant_id           = var.azure_tenant_id
   subscription_id     = var.azure_subscription_id
 }
+
+provider "azapi" {
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+}
